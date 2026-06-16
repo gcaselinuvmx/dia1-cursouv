4685d21341a25a4af80cf9af0c8a7c32399268ef
Gilberto Caselin <gcaselin@uv.mx>
2026-06-16 11:02:54 -0600

    Agregar boostarp 5 a la página y un mensaje con js

---
+++ README.md
@@ -0,0 +1,9 @@
+#Mi proyecto inicial para mi curso de git
+
+Repositorio didáctico del curso **Control de versiones profesional con git**
+
+##estructura 
+
+##autor
+
+Gil
--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 console.log("Hola mundo desde js de git");
+
+        document.getElementById('miAlerta').addEventListener('click', function() {
+            alert('¡Hola! Esta es una alerta de JavaScript.');
+        });
